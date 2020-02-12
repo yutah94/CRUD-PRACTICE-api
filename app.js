906677fb7fbcd0d70892b99e815d0440ec99dfb9
yutah94 <yuta.young@gmail.com>
2020-02-12 15:21:20 -0700

    Updated 2/12/2020

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ var logger = require('morgan');
 var bookRouter = require('./routes/api');
 var usersRouter = require('./routes/users');
 require('dotenv').config
+const cors = require('cors');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -33,7 +35,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
